69522149e4bb3de4d24f3e27338ec2c5877bc97a
Radim Ciz <radimciz@yahoo.com>
2025-06-05 20:37:59 +0200

    cucumber framework start

--- src/test/java/UiTests/Pages/PageManager.java
+++ src/test/java/UiTests/Pages/PageManager.java
@@ -1,4 +1,16 @@
 package UiTests.Pages;
 
+import UiTests.Steps.TestContext;
+
 public class PageManager {
+    private final TestContext context;
+    private BasePage basePage;
+
+    public PageManager(TestContext context) {
+        this.context = context;
+    }
+
+    public BasePage getBasePage() {
+        return (basePage == null) ? new BasePage(context) : basePage;
+    }
 }
--- src/test/java/UiTests/Steps/Hooks.java
+++ src/test/java/UiTests/Steps/Hooks.java
@@ -1,4 +1,25 @@
 package UiTests.Steps;
 
+import UiTests.Pages.PageManager;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Hooks {
+    private final TestContext context;
+
+    public Hooks(TestContext context) {
+        this.context = context;
+    }
+
+    @Before()
+    public void before(){
+      context.driver = new ChromeDriver();
+      context.pm = new PageManager(context);
+    }
+
+    @After()
+    public void after(){
+        context.driver.quit();
+    }
 }
