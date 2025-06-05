25d37bbb6068851fb2d473759d2ab119782a40a4
Radim Ciz <radimciz@yahoo.com>
2025-06-05 20:16:42 +0200

    repo framework

---
+++ src/test/java/UiTests/Runners/TestRunner.java
@@ -0,0 +1,17 @@
+package UiTests.Runners;
+
+import io.cucumber.core.options.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("Features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "UiTests/Steps")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@regression")
+@ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
+public class TestRunner {
+}
