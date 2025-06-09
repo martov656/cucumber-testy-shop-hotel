33bf67ece9789a3098114b9fb0641880691be1de
Radim Ciz <radimciz@yahoo.com>
2025-06-09 19:01:15 +0200

    cucumber framework scenarios

--- src/test/java/UiTests/Pages/Hotel/HotelHomePage.java
+++ src/test/java/UiTests/Pages/Hotel/HotelHomePage.java
@@ -2,6 +2,8 @@ package UiTests.Pages.Hotel;
 
 import UiTests.Pages.BasePage;
 import UiTests.Steps.TestContext;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HotelHomePage extends BasePage {
@@ -10,9 +12,23 @@ public class HotelHomePage extends BasePage {
         PageFactory.initElements(context.driver, this);
     }
 
+    // *********************** Elements *************************************
+
+    @FindBy(xpath = "//span[text()='Sign in']")
+    WebElement signInLink;
+
+
+
+
+    // *********************** Methods **************************************
 
     public void loadHomePage() {
         driver.manage().window().maximize();
         driver.get(context.configProperties.getProperty("hotelUrl"));
     }
+
+    public void clickSignIn() {
+        this.clickWebElement(this.signInLink);
+    }
+
 }
