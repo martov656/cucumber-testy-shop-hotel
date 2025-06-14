6accb5ce3f93974c77c1254fe0de379187835563
Radim Ciz <radimciz@yahoo.com>
2025-06-15 01:15:11 +0200

    cucumber framework ghw

--- src/test/java/UiTests/Steps/Hooks.java
+++ src/test/java/UiTests/Steps/Hooks.java
@@ -3,8 +3,12 @@ package UiTests.Steps;
 import UiTests.Pages.PageManager;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.maven.surefire.shared.io.FileUtils;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Point;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -17,6 +21,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Properties;
 
 public class Hooks {
@@ -49,8 +56,11 @@ public class Hooks {
     }
 
     @After()
-    public void after() {
+    public void after(Scenario scenario) {
         waitBeforeClosing_ForLecturerDemoOnly();
+        if (scenario.isFailed()) {
+            this.takeScreenshot();
+        }
         context.driver.quit();
     }
 
@@ -100,6 +110,23 @@ public class Hooks {
             inLecturerPc = true;
         }
         return inLecturerPc;
+    }
+
+
+    private void takeScreenshot() {
+        String screenshotDir = System.getProperty("user.dir") + File.separator + "screenshots";
+        File directory = new File(screenshotDir);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+        File scrFile = ((TakesScreenshot) context.driver).getScreenshotAs(OutputType.FILE);
+        // System.out.println(System.getProperty("user.dir") + File.separator + "screenshots" + File.separator + "screenshot_" + timeStamp + ".png");
+        try {
+            FileUtils.copyFile(scrFile, new File(screenshotDir + File.separator + "screenshot_" + timeStamp + ".png"), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
